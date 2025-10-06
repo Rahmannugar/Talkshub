48be033515b7d38f4a824d2cb75b056321f0622b
Rahman Nugar <cladeadenugar@gmail.com>
2025-10-06 15:51:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,6 @@ if (close) {
         nav.classList.remove("active")
     })
 }
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDIpmWcYyNQvQtlNJbnJaiMbrfFQfhEZV0",
-    authDomain: "talkshub-81f64.firebaseapp.com",
-    projectId: "talkshub-81f64",
-    storageBucket: "talkshub-81f64.appspot.com",
-    messagingSenderId: "759345416835",
-    appId: "1:759345416835:web:4b81afa38c4eb6197269b6",
-    measurementId: "G-0YEH2S02PL"
-  };
   function login(event){
   }
   function logout(){
